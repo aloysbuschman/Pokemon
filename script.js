49be314b60e0f0661e79e58e293f82bd6f62f717
aloysbuschman <aloys.buschman@gmail.com>
2022-08-29 17:19:12 +0200

    Add basic styling

--- script.js
+++ script.js
@@ -3,21 +3,26 @@ const wrapper = document.querySelector('#wrapper');
 
 async function fetchPokemon() {
   let html = ''
-  for (let i = 1; i <= 6; i++) {
+  for (let i = 1; i <= 15; i++) {
     const response =  await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
     const data =  await response.json(); 
+    
     const pokeDetails = {
       id: data.id,
       name: data.name,
       img: data.sprites.front_default,
       type: data.types[0].type.name
     };
-    console.log(pokeDetails);
+
     html += `
       <div class="pokemon-card" data-pokemon-id="${pokeDetails.id}">
-        <h3 class="pokemon-name">${pokeDetails.name}</h3>
-        <img src="${pokeDetails.img}">
-        <span>Type: ${pokeDetails.type} </span>
+        <div class="img-wrapper">
+          <img class="pokemon-img" src="${pokeDetails.img}">
+        </div>
+        <div class="details-wrapper">
+          <h3 class="pokemon-name">${pokeDetails.name}</h3>
+          <span>Type: ${pokeDetails.type} </span>
+        </div>
       </div>
   `
   wrapper.innerHTML = html;
